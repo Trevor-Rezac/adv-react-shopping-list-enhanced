10d30ba3ff022485ab60d45ed2feac6454811f37
TrevorRezac <trev.rezac@gmail.com>
2022-05-13 11:17:48 -0700

    passing behavioral test for user adding a new item

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import userEvent from '@testing-library/user-event';
+import { ShoppingListProvider } from './context/ShoppingListProvider';
+
 describe('App tests', () => {
-  it('should be a passing test', () => {
-    expect(true).toEqual(true);
+  it('should be a passing test', async () => {
+    render(
+        <App />
+    )
+    
+    //tests that the header is present
+    const appHeader = screen.getByText(/Total Items:/i)
+    expect(appHeader).toBeInTheDocument();
+    
+    //tests that the title is present
+    const mainTitle = screen.getByText(/Shopping List Items/i);
+    expect(mainTitle).toBeInTheDocument();
+    
+    //tests the user typing into the item input field then pressing "enter" to submit
+    //if not typing {enter} after the text in the input field, I would need to use a fireEvent to submit the form
+    const itemInput = screen.getByPlaceholderText('Add new item');
+    userEvent.type(itemInput, 'food{enter}');
+    
+    //confirms that one item with text 'food' appears on page after user submits form
+    const newItem = await screen.findAllByText('food');
+    expect(newItem.length).toBe(1);
   })
 })
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { ShoppingListProvider } from './context/ShoppingListProvider';
 
 render(
   <React.StrictMode>
-    <App />
+    <ShoppingListProvider>
+      <App />
+    </ShoppingListProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
